63153e24b5d2c89552403d3d668a00bace700e2a
Jan Eric Lenssen <janeric.lenssen@tu-dortmund.de>
2022-07-08 17:52:44 +0200

    eccv oral

--- index.11tydata.js
+++ index.11tydata.js
@@ -10,7 +10,7 @@ module.exports = {
             title: "Pose-NDF: Modeling Human Pose Manifolds with Neural Distance Fields",
             teaser: "figures/posendf.png",
             authors: "Garvita Tiwari, Dimitrije Antic, Jan Eric Lenssen, Nikolaos Sarafianos, Tony Tung, Gerard Pons-Moll",
-            conference: "ECCV 2022",
+            conference: "ECCV 2022 (Oral presentation)",
             //data: ["[Project Page]".link("https://virtualhumans.mpi-inf.mpg.de/toch/"), "[Paper]".link("https://arxiv.org/pdf/2205.07982.pdf")].join(" "),
             abstract: "An unsigned neural distance field that models the manifold of plausible human poses in high-dimensional SO(3). Given human poses can be projected onto the manifold by SO(3) gradient descent.",
         },
